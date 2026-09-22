13c32a6a07bc08299e9b4fe72446ee53114a243b
Nixon <167166401+Nixon-CSE@users.noreply.github.com>
2026-09-22 09:14:40 +0530

    Time: 3 ms (99.82%), Space: 46.6 MB (13.84%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] sym = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder res = new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            while(num >= values[i]) {
+                res.append(sym[i]);
+                num -= values[i];
+            }
+            if(num == 0){
+                break;
+            }
+        }
+
+        return res.toString();
+
+
+    }
+}
